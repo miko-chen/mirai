71bd2a60a86414fe6492bd80015d18fee3100210
Him188 <Him188@mamoe.net>
2022-05-19 15:36:55 +0100

    ImageProtocolTest for messages received from Android and iOS clients

--- mirai-core/src/commonTest/kotlin/message/protocol/impl/ImageProtocolTest.kt
+++ mirai-core/src/commonTest/kotlin/message/protocol/impl/ImageProtocolTest.kt
@@ -34,7 +34,7 @@ internal class ImageProtocolTest : AbstractMessageProtocolTest() {
     ///////////////////////////////////////////////////////////////////////////
 
     @Test
-    fun `group Image receive from macOS client`() {
+    fun `group Image receive from macOS`() {
         buildChecks {
             elem(
                 net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.Elem(
@@ -63,11 +63,13 @@ internal class ImageProtocolTest : AbstractMessageProtocolTest() {
                 type = ImageType.JPG
                 isEmoji = false
             })
+            targetGroup()
+            useOrdinaryEquality()
         }.doDecoderChecks()
     }
 
     @Test
-    fun `friend Image receive from macOS client`() {
+    fun `friend Image receive from macOS`() {
         buildChecks {
             elem(
                 net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.Elem(
@@ -97,6 +99,173 @@ internal class ImageProtocolTest : AbstractMessageProtocolTest() {
                 type = ImageType.JPG
                 isEmoji = false
             })
+            targetFriend()
+        }.doDecoderChecks()
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // receive from Android
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Test
+    fun `group Image receive from Android`() {
+        buildChecks {
+            elem(
+                net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.Elem(
+                    customFace = net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.CustomFace(
+                        filePath = "A7CBB52943A2127CE42659D29BAA8515.jpg",
+                        oldData = "15 36 20 38 36 65 41 31 42 61 66 34 35 64 37 38 63 36 66 31 65 39 30 33 66 20 20 20 20 20 20 35 30 57 4A 4B 53 53 71 52 79 61 52 46 42 7A 77 38 34 41 37 43 42 42 35 32 39 34 33 41 32 31 32 37 43 45 34 32 36 35 39 44 32 39 42 41 41 38 35 31 35 2E 6A 70 67 41".hexToBytes(),
+                        fileId = -1354377332,
+                        serverIp = 1864273983,
+                        serverPort = 80,
+                        fileType = 66,
+                        signature = "WJKSSqRyaRFBzw84".toByteArray(), /* 57 4A 4B 53 53 71 52 79 61 52 46 42 7A 77 38 34 */
+                        useful = 1,
+                        picMd5 = "A7 CB B5 29 43 A2 12 7C E4 26 59 D2 9B AA 85 15".hexToBytes(),
+                        thumbUrl = "/gchatpic_new/123456/622457678-2940589964-A7CBB52943A2127CE42659D29BAA8515/198?term=2",
+                        origUrl = "/gchatpic_new/123456/622457678-2940589964-A7CBB52943A2127CE42659D29BAA8515/0?term=2",
+                        imageType = 1000,
+                        width = 904,
+                        height = 1214,
+                        source = 103,
+                        size = 170426,
+                        thumbWidth = 147,
+                        thumbHeight = 198,
+                        _400Url = "/gchatpic_new/123456/622457678-2940589964-A7CBB52943A2127CE42659D29BAA8515/400?term=2",
+                        _400Width = 285,
+                        _400Height = 384,
+                        pbReserve = "08 01 10 00 32 00 4A 0E 5B E5 8A A8 E7 94 BB E8 A1 A8 E6 83 85 5D 50 00 78 06".hexToBytes(),
+                    ),
+                )
+            )
+            message(Image("{A7CBB529-43A2-127C-E426-59D29BAA8515}.jpg") {
+                width = 904
+                height = 1214
+                size = 170426
+                type = ImageType.JPG
+                isEmoji = false
+            })
+            targetGroup()
+            useOrdinaryEquality()
+        }.doDecoderChecks()
+    }
+
+    @Test
+    fun `friend Image receive from Android`() {
+        buildChecks {
+            elem(
+                net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.Elem(
+                    notOnlineImage = net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.NotOnlineImage(
+                        filePath = "A7CBB52943A2127CE42659D29BAA8515.jpg",
+                        fileLen = 170426,
+                        downloadPath = "/123456-113241016-A7CBB52943A2127CE42659D29BAA8515",
+                        oldVerSendFile = "16 20 31 31 36 31 30 31 30 35 31 41 42 20 20 20 20 31 37 30 34 32 36 65 41 37 43 42 42 35 32 39 34 33 41 32 31 32 37 43 45 34 32 36 35 39 44 32 39 42 41 41 38 35 31 35 2E 6A 70 67 77 2F 31 30 34 30 34 30 30 32 39 30 2D 31 31 33 32 34 31 30 31 36 2D 41 37 43 42 42 35 32 39 34 33 41 32 31 32 37 43 45 34 32 36 35 39 44 32 39 42 41 41 38 35 31 35 41".hexToBytes(),
+                        imgType = 1000,
+                        picMd5 = "A7 CB B5 29 43 A2 12 7C E4 26 59 D2 9B AA 85 15".hexToBytes(),
+                        picHeight = 1214,
+                        picWidth = 904,
+                        resId = "/123456-113241016-A7CBB52943A2127CE42659D29BAA8515",
+                        thumbUrl = "/offpic_new/123456//123456-113241016-A7CBB52943A2127CE42659D29BAA8515/198?term=2",
+                        origUrl = "/offpic_new/123456//123456-113241016-A7CBB52943A2127CE42659D29BAA8515/0?term=2",
+                        bizType = 5,
+                        thumbWidth = 147,
+                        thumbHeight = 198,
+                        _400Url = "/offpic_new/123456//123456-113241016-A7CBB52943A2127CE42659D29BAA8515/400?term=2",
+                        _400Width = 285,
+                        _400Height = 384,
+                        pbReserve = "08 01 10 00 32 00 42 0E 5B E5 8A A8 E7 94 BB E8 A1 A8 E6 83 85 5D 50 00 78 06".hexToBytes(),
+                    ),
+                )
+            )
+            message(Image("{A7CBB529-43A2-127C-E426-59D29BAA8515}.jpg") {
+                width = 904
+                height = 1214
+                size = 170426
+                type = ImageType.JPG
+                isEmoji = false
+            })
+            targetFriend()
+        }.doDecoderChecks()
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // receive from iOS
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Test
+    fun `group Image receive from iOS`() {
+        buildChecks {
+            elem(
+                net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.Elem(
+                    notOnlineImage = net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.NotOnlineImage(
+                        filePath = "C344D6240014DA35BB63A958BC435134.png",
+                        fileLen = 108536,
+                        downloadPath = "/123456-346835805-C344D6240014DA35BB63A958BC435134",
+                        oldVerSendFile = "16 20 31 31 36 31 30 31 30 35 31 41 42 20 20 20 20 31 30 38 35 33 36 65 43 33 34 34 44 36 32 34 30 30 31 34 44 41 33 35 42 42 36 33 41 39 35 38 42 43 34 33 35 31 33 34 2E 70 6E 67 77 2F 32 36 35 32 33 38 36 32 32 38 2D 33 34 36 38 33 35 38 30 35 2D 43 33 34 34 44 36 32 34 30 30 31 34 44 41 33 35 42 42 36 33 41 39 35 38 42 43 34 33 35 31 33 34 41".hexToBytes(),
+                        imgType = 1000,
+                        picMd5 = "C3 44 D6 24 00 14 DA 35 BB 63 A9 58 BC 43 51 34".hexToBytes(),
+                        picHeight = 1214,
+                        picWidth = 904,
+                        resId = "/123456-346835805-C344D6240014DA35BB63A958BC435134",
+                        thumbUrl = "/offpic_new/123456//123456-346835805-C344D6240014DA35BB63A958BC435134/198?term=2",
+                        origUrl = "/offpic_new/123456//123456-346835805-C344D6240014DA35BB63A958BC435134/0?term=2",
+                        bizType = 4,
+                        thumbWidth = 147,
+                        thumbHeight = 198,
+                        _400Url = "/offpic_new/123456//123456-346835805-C344D6240014DA35BB63A958BC435134/400?term=2",
+                        _400Width = 285,
+                        _400Height = 384,
+                        pbReserve = "08 00 10 00 18 00 50 00 78 04".hexToBytes(),
+                    ),
+                )
+            )
+            message(Image("{A7CBB529-43A2-127C-E426-59D29BAA8515}.jpg") {
+                width = 904
+                height = 1214
+                size = 170426
+                type = ImageType.JPG
+                isEmoji = false
+            })
+            targetGroup()
+        }.doDecoderChecks()
+    }
+
+    @Test
+    fun `friend Image receive from iOS`() {
+        buildChecks {
+            elem(
+                net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.Elem(
+                    notOnlineImage = net.mamoe.mirai.internal.network.protocol.data.proto.ImMsgBody.NotOnlineImage(
+                        filePath = "C344D6240014DA35BB63A958BC435134.png",
+                        fileLen = 108536,
+                        downloadPath = "/123455-346835805-C344D6240014DA35BB63A958BC435134",
+                        oldVerSendFile = "16 20 31 31 36 31 30 31 30 35 31 41 42 20 20 20 20 31 30 38 35 33 36 65 43 33 34 34 44 36 32 34 30 30 31 34 44 41 33 35 42 42 36 33 41 39 35 38 42 43 34 33 35 31 33 34 2E 70 6E 67 77 2F 32 36 35 32 33 38 36 32 32 38 2D 33 34 36 38 33 35 38 30 35 2D 43 33 34 34 44 36 32 34 30 30 31 34 44 41 33 35 42 42 36 33 41 39 35 38 42 43 34 33 35 31 33 34 41".hexToBytes(),
+                        imgType = 1000,
+                        picMd5 = "C3 44 D6 24 00 14 DA 35 BB 63 A9 58 BC 43 51 34".hexToBytes(),
+                        picHeight = 1214,
+                        picWidth = 904,
+                        resId = "/123455-346835805-C344D6240014DA35BB63A958BC435134",
+                        thumbUrl = "/offpic_new/123455//123455-346835805-C344D6240014DA35BB63A958BC435134/198?term=2",
+                        origUrl = "/offpic_new/123455//123455-346835805-C344D6240014DA35BB63A958BC435134/0?term=2",
+                        bizType = 4,
+                        thumbWidth = 147,
+                        thumbHeight = 198,
+                        _400Url = "/offpic_new/123455//123455-346835805-C344D6240014DA35BB63A958BC435134/400?term=2",
+                        _400Width = 285,
+                        _400Height = 384,
+                        pbReserve = "08 00 10 00 18 00 50 00 78 04".hexToBytes(),
+                    ),
+                )
+            )
+            message(Image("{C344D624-0014-DA35-BB63-A958BC435134}.jpg") {
+                width = 904
+                height = 1214
+                size = 108536
+                type = ImageType.JPG
+                isEmoji = false
+            })
+            targetFriend()
+            useOrdinaryEquality()
         }.doDecoderChecks()
     }
 
